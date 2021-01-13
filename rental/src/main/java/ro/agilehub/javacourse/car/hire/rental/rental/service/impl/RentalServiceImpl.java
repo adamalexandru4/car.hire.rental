f0447c3880a5ced971bb8dd8b225eafd0b7dbc1b
Adam Alexandru <adamalexandru4@yahoo.com>
2021-01-13 15:33:39 +0200

    Functional rental service with feign support

--- rental/src/main/java/ro/agilehub/javacourse/car/hire/rental/rental/service/impl/RentalServiceImpl.java
+++ rental/src/main/java/ro/agilehub/javacourse/car/hire/rental/rental/service/impl/RentalServiceImpl.java
@@ -1,15 +1,21 @@
 package ro.agilehub.javacourse.car.hire.rental.rental.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ro.agilehub.javacourse.car.hire.rental.api.model.NewReservationDTO;
 import ro.agilehub.javacourse.car.hire.rental.api.model.ResourceCreatedDTO;
+import ro.agilehub.javacourse.car.hire.rental.client.core.specification.UserApi;
 import ro.agilehub.javacourse.car.hire.rental.rental.service.RentalService;
 
 @Service
+@RequiredArgsConstructor
 public class RentalServiceImpl implements RentalService {
+
+    private final UserApi userApi;
 
     @Override
     public ResourceCreatedDTO createReservation(NewReservationDTO newReservationDTO) {
-        return null;
+        var getUserResponseEntity = userApi.getUserByID("5ffedb0e70d4c67cbf157000");
+        return new ResourceCreatedDTO().id(getUserResponseEntity.getBody().getId());
     }
 }
